34b23419fb3dc807f412cbf4ac8bc1941dc5b9a0
ValeryaTretyakova <124460665+ValeryaTretyakova@users.noreply.github.com>
2023-05-29 14:08:40 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.mult(4, 2));
+        System.out.println(calc.sum(18, 30));
+        System.out.println(calc.pow(3, 3));
+    }
+}
